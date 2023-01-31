900ed77ccca1697503889b35b7a56f0a90f0e9a7
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-02-01 01:05:04 +0300

    ALGORITHM: fix bug in macro

--- algorithm/algorithm_def.h
+++ algorithm/algorithm_def.h
@@ -14,7 +14,7 @@
 //--------------------------------------------------------------------------------------------------------------------------------
 
 #define buffer_ctor(     buff, buff_size) buffer_ctor     (__FILE__, __PRETTY_FUNCTION__, __LINE__, buff, buff_size)
-#define buffer_ctor_file(buff, file_name) buffer_ctor_file(__FILE__, __PRETTY_FINCTION__, __LINE__, buff, file_name)
+#define buffer_ctor_file(buff, file_name) buffer_ctor_file(__FILE__, __PRETTY_FUNCTION__, __LINE__, buff, file_name)
 #define buffer_dtor(     buff           ) buffer_dtor     (__FILE__, __PRETTY_FUNCTION__, __LINE__, buff           )
 
 #endif //ALG_DEF_H
