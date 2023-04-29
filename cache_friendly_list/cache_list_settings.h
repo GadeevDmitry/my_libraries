be82ee7ec8852aa6c8b7a21056050be4e9b8517b
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-04-30 01:34:07 +0300

    add cache_list_settings

---
+++ cache_friendly_list/cache_list_settings.h
@@ -0,0 +1,17 @@
+#ifndef CACHE_LIST_SETTINGS_H
+#define CACHE_LIST_SETTINGS_H
+
+//#define NDEBUG
+//#define NVERIFY
+
+//#define LOG_NTRACE
+#define LOG_NLEAK
+//#define LOG_NDEBUG
+//#define LOG_NVERIFY
+
+//#define LIST_NVERIFY
+//#define LIST_NDEBUG
+
+const size_t DEFAULT_LIST_CAPACITY = 4;
+
+#endif //CACHE_LIST_SETTINGS_H
