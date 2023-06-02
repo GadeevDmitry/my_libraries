9f050a1d8bfdc3dfb7cc2013e50a1d37f5bc38eb
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-06-02 13:30:15 +0300

    add struct array

---
+++ array/array_settings.h
@@ -0,0 +1,16 @@
+#ifndef ARRAY_SETTINGS_H
+#define ARRAY_SETTINGS_H
+
+#define NDEBUG
+#define NVERIFY
+
+#define LOG_NTRACE
+#define LOG_NLEAK
+//#define NLOG
+//#define LOG_NDEBUG
+//#define LOG_NVERIFY
+
+//#define ARRAY_NVERIFY
+//#define ARRAY_NDEBUG
+
+#endif //ARRAY_SETTINGS_H
