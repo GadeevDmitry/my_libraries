0b6ea024238787f4aa9dd2ebad5575598cd7f1ab
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-07-07 22:28:24 +0300

    set ndebug list mode

--- list/list_settings.h
+++ list/list_settings.h
@@ -1,11 +1,11 @@
 #ifndef LIST_SETTINGS_H
 #define LIST_SETTINGS_H
 
-//#define NDEBUG
-//#define NVERIFY
+#define NDEBUG
+#define NVERIFY
 
-//#define LOG_NTRACE
-//#define LOG_NLEAK
+#define LOG_NTRACE
+#define LOG_NLEAK
 //#define LOG_NDEBUG
 //#define LOG_NVERIFY
 
